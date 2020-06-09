9f8497678f4aed85c48e4859e3d96a65294e4400
Ying_Chen <emily1017c@gmail.com>
2020-06-10 00:56:03 +0800

    try
    12.55

---
+++ js/36.7f3a8eb7.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([[36],{"2c0a":function(e,t,n){"use strict";n.r(t);var r=function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div")},a=[],u={name:"emptyPage",beforeRouteEnter(e,t,n){n(e=>{e.$router.replace(t.path)})}},c=u,l=n("2877"),o=Object(l["a"])(c,r,a,!1,null,null,null);t["default"]=o.exports}}]);
